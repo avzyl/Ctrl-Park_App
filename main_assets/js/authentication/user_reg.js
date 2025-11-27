cd6cc085156118d4b8718a179a0bd3105d0b430d
Lyzza Abig <lrs.abig@yahoo.com>
2025-11-28 03:00:33 +0800

    Email

--- main_assets/js/authentication/user_reg.js
+++ main_assets/js/authentication/user_reg.js
@@ -371,7 +371,7 @@ if (resetLink) {
       });
 
       // 4️⃣ Build the actual reset link (update with your real URL)
-      const resetLink = `http://127.0.0.1:5501/reset_password.html?token=${token}&id=${userId}`;
+      const resetLink = `https://avzyl.github.io/Ctrl-Park_App/reset_password.html?token=${reset_token}&id=${user_id}`;
 
       // 5️⃣ Send email using EmailJS
       emailjs.init("SKq6rRh-aPDV4uugA");  // 🔑 Your EmailJS public key
